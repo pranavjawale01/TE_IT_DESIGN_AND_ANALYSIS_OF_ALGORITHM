7066d352f0d89ff0fe67d1bfd85f1c841502d88a
Pranav Jawale <98261249+pranavjawale01@users.noreply.github.com>
2023-11-04 12:19:05 +0530

    Update DAAZO01.cpp

--- DAAZO01.cpp
+++ DAAZO01.cpp
@@ -3,6 +3,125 @@
 #include<iostream>
 using namespace std;
 
+void input(int totalItem, int bagWeight, int table[][15], int profit[], int weight[]) {
+    for (int i = 1; i <= totalItem; i++) {
+        cout << "\nEnter the Weight of " << i << " item >> ";
+        cin >> weight[i];
+        cout << "\nEnter the Profit of " << i << " item >> ";
+        cin >> profit[i];
+    }
+    for (int i = 0; i <= totalItem; i++) {
+        for (int j = 0; j<= bagWeight; j++) {
+            table[i][j] = 0;
+        }
+    }
+}
+
+void display(int table[][15], int totalItem, int bagWeight) {
+    for (int i = 0 ; i <= totalItem; i++) {
+        for (int j = 0 ; j <= bagWeight; j++) {
+            cout<<table[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+}
+
+void calculate(int table[][15], int totalItem, int bagWeight, int profit[], int weight[]) {
+    for (int i = 1; i <= totalItem; i++) {
+        for (int j = 1; j <= bagWeight; j++) {
+            table[i][j] = table[i-1][j];
+            if (weight[i] <= j) {
+                table[i][j] = max(table[i][j], profit[i] + table[i-1][j-weight[i]]);
+            }
+        }
+    }
+}
+
+void selectItem(int totalItem, int bagWeight, int table[][15], int weight[], int profit[]) {
+    int i = totalItem;
+    int j = bagWeight;
+    int totalWeight = 0;
+    int totalProfit = 0;
+    while (i > 0 && j > 0) {
+        if (table[i][j] != table[i-1][j]) {
+            cout<<"Item "<<i <<" selected" << endl;
+            totalWeight += weight[i];
+            totalProfit += profit[i];
+            j -= weight[i];
+        }
+        i--;
+    }
+    cout << "Total Weight of selected items: " << totalWeight << endl;
+    cout << "Total Profit of selected items: " << totalProfit << endl;
+}
+
+int main() {
+    int bagWeight, totalItem;
+    cout << "Enter the Weight of KnapSack Bag >> ";
+    cin >> bagWeight;
+    cout << "Enter the Total Number of Items >> ";
+    cin >> totalItem;
+    int table[totalItem + 1][15];
+    int profit[totalItem + 1], weight[totalItem + 1];
+    input(totalItem, bagWeight, table, profit, weight);
+    calculate(table, totalItem, bagWeight, profit, weight);
+    cout << "\nDynamic Programming Table:" << endl;
+    display(table, totalItem, bagWeight);
+    cout << "\nSelected items:" << endl;
+    selectItem(totalItem, bagWeight, table, weight, profit);
+
+    return 0;
+}
+
+// OUTPUT >>
+// Enter the Weight of KnapSack Bag >> 8
+
+// Enter the Total Number of Items >> 4
+
+// Enter the Weight of 1 item >> 2
+
+// Enter the Profit of 1 item >> 1
+
+// Enter the Weight of 2 item >> 3
+
+// Enter the Profit of 2 item >> 2
+
+// Enter the Weight of 3 item >> 4
+
+// Enter the Profit of 3 item >> 5
+
+// Enter the Weight of 4 item >> 5
+
+// Enter the Profit of 4 item >> 6
+// Displaying 0/1 KnapSack Table...
+// 0       0       0       0       0       0       0       0       0
+// 0       0       0       0       0       0       0       0       0
+// 0       0       0       0       0       0       0       0       0
+// 0       0       0       0       0       0       0       0       0
+// 0       0       0       0       0       0       0       0       0
+
+// Table after data allocation
+// Displaying 0/1 KnapSack Table...
+// 0       0       0       0       0       0       0       0       0
+// 0       0       1       1       1       1       1       1       1
+// 0       0       1       2       2       3       3       3       3
+// 0       0       1       2       5       5       6       7       7
+// 0       0       1       2       5       6       6       7       8
+
+// Items selected:
+// Item 4 selected
+// Item 2 selected
+
+// Total Weight of selected items: 8
+// Total Profit of selected items: 8
+
+
+
+//.....Using Class.....
+/*
+#include<iostream>
+using namespace std;
+
 class zeroOneKnapSack
 {
     private:
@@ -118,45 +237,4 @@ int main()
 
     return 0;
 }
-
-// OUTPUT >>
-// Enter the Weight of KnapSack Bag >> 8
-
-// Enter the Total Number of Items >> 4
-
-// Enter the Weight of 1 item >> 2
-
-// Enter the Profit of 1 item >> 1
-
-// Enter the Weight of 2 item >> 3
-
-// Enter the Profit of 2 item >> 2
-
-// Enter the Weight of 3 item >> 4
-
-// Enter the Profit of 3 item >> 5
-
-// Enter the Weight of 4 item >> 5
-
-// Enter the Profit of 4 item >> 6
-// Displaying 0/1 KnapSack Table...
-// 0       0       0       0       0       0       0       0       0
-// 0       0       0       0       0       0       0       0       0
-// 0       0       0       0       0       0       0       0       0
-// 0       0       0       0       0       0       0       0       0
-// 0       0       0       0       0       0       0       0       0
-
-// Table after data allocation
-// Displaying 0/1 KnapSack Table...
-// 0       0       0       0       0       0       0       0       0
-// 0       0       1       1       1       1       1       1       1
-// 0       0       1       2       2       3       3       3       3
-// 0       0       1       2       5       5       6       7       7
-// 0       0       1       2       5       6       6       7       8
-
-// Items selected:
-// Item 4 selected
-// Item 2 selected
-
-// Total Weight of selected items: 8
-// Total Profit of selected items: 8
+*/
